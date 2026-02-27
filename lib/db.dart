4acef51a9f5781db17915ae1025eabd2b7a022f1
John Suyang <johnsuyang@gmail.com>
2026-02-27 15:32:59 +0800

    db update

--- lib/db.dart
+++ lib/db.dart
@@ -5,8 +5,8 @@ import "package:path_provider/path_provider.dart";
 Future<Database> db() async {
   final appDocumentDir = await getApplicationDocumentsDirectory();
   final database = sqlite3.open(join(appDocumentDir.path, 'questions2.db'));
-  // database.execute(
-  //   "CREATE TABLE questions4 (id varchar(64), question text, answer1 text, answer2 text, answer3 text, answer4 text, correctAnswer text )",
-  // );
+  database.execute(
+    "CREATE TABLE if not exists questions4 (id varchar(64), question text, answer1 text, answer2 text, answer3 text, answer4 text, correctAnswer text )",
+  );
   return database;
 }
