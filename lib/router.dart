b4fb650f213a718a212ae0cbe51853a8e58c6384
John Suyang <johnsuyang@gmail.com>
2026-02-27 15:11:03 +0800

    updates

--- lib/router.dart
+++ lib/router.dart
@@ -6,7 +6,7 @@ import 'package:flutter_friday_test/screens/questions/questions_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
-  initialLocation: QuestionsScreen.routeName,
+  initialLocation: HomeScreen.routeName,
   routes: [
     GoRoute(
       path: HomeScreen.routeName,
